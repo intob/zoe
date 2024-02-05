c18ff5758c62b38a5f64842a455f44d9c210f167
Joey Innes <avianpneuma@gmail.com>
2024-02-06 00:51:36 +0100

    Tidy & fix hopefully

--- update-aws.sh
+++ update-aws.sh
@@ -4,23 +4,8 @@
 # Update AWS Route53 DNS records for lstn.swissinfo.ch
 #
 
-RECORD_NAME=$1
-
 echo "Updating DNS records for $RECORD_NAME in hosted zone $AWS_HOSTED_ZONE_ID"
-
-while getopts ":ipv4:ipv6:" opt; do
-  case $opt in
-    ipv4)
-      IPV4_ADDRESS=$OPTARG
-      ;;
-    ipv6)
-      IPV6_ADDRESS=$OPTARG
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      ;;
-  esac
-done
+echo "IPv4: $IPV4 / IPv6: $IPV6"
 
 # Check if records exist
 AAAA_RECORDS=$(aws route53 list-resource-record-sets --hosted-zone-id $AWS_HOSTED_ZONE_ID --query "ResourceRecordSets[?Type == 'AAAA'] | [?contains(Name, '$RECORD_NAME')]")
@@ -33,7 +18,7 @@ if [ "$AAAA_RECORDS" == "[]" ]; then
   echo "AAAA record does not exist. Creating..."
   aws route53 change-resource-record-sets \
       --hosted-zone-id $AWS_HOSTED_ZONE_ID \
-      --change-batch "{\"Changes\":[{\"Action\":\"UPSERT\",\"ResourceRecordSet\":{\"Name\":\"$RECORD_NAME\",\"Type\":\"AAAA\",\"TTL\":60,\"ResourceRecords\":[{\"Value\":\"$IPV6_ADDRESS\"}]}}]}"
+      --change-batch "{\"Changes\":[{\"Action\":\"UPSERT\",\"ResourceRecordSet\":{\"Name\":\"$RECORD_NAME\",\"Type\":\"AAAA\",\"TTL\":60,\"ResourceRecords\":[{\"Value\":\"$IPV6\"}]}}]}"
 else
   echo "AAAA record exists. Not updating."
 fi
@@ -42,7 +27,7 @@ if [ "$A_RECORDS" == "[]" ]; then
   echo "A record does not exist. Creating..."
   aws route53 change-resource-record-sets \
       --hosted-zone-id $AWS_HOSTED_ZONE_ID \
-      --change-batch "{\"Changes\":[{\"Action\":\"UPSERT\",\"ResourceRecordSet\":{\"Name\":\"$RECORD_NAME\",\"Type\":\"A\",\"TTL\":60,\"ResourceRecords\":[{\"Value\":\"$IPV4_ADDRESS\"}]}}]}"
+      --change-batch "{\"Changes\":[{\"Action\":\"UPSERT\",\"ResourceRecordSet\":{\"Name\":\"$RECORD_NAME\",\"Type\":\"A\",\"TTL\":60,\"ResourceRecords\":[{\"Value\":\"$IPV4\"}]}}]}"
 else
   echo "A record exists. Not updating."
 fi
