8fe0b7dcfadb51fef2a0abf7e53da81eb2191205
Joey Innes <avianpneuma@gmail.com>
2024-02-07 10:21:26 +0100

    js link in root

--- app/root.go
+++ app/root.go
@@ -24,7 +24,7 @@ func (a *App) handleRoot(w http.ResponseWriter, r *http.Request) {
 		<p>Send events to <code>/</code> with headers <code>X_TYPE</code>, <code>X_USR</code>, <code>X_SESS</code> and <code>X_CID</code>.</p>
 		<p>Get reports from <code>/r</code> with query parameter <code>name</code>:</p>
 		<ul>` + listItemsHTML + `</ul>
-		<p>Get client-side script from <code>/js</code>.</p>
+		<p>Get client-side script from <a href="/js">/js</a>.</p>
 		<p>See <a href="https://github.com/swissinfo-ch/lstn">github.com/swissinfo-ch/lstn</a> for more information.</p>
 	</body>
 </html>`
